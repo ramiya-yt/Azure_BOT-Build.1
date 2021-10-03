0ce866bc0ccb05900d6501e3091bd025d9b0aab9
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2021-10-03 16:34:13 +0530

    Create ramiya.js

---
+++ ramiya.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('ramiya.env')) require('dotenv').hatzu({ path: './ramiya.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+
+module.exports = {
+  VERSION: 'v1.0.0',
+  ADMIN: process.env.ADMIN === undefined ?'RAMIYA' : process.env.ADMIN,
+  ADMIN1: process.env.ADMIN1 === undefined ?'RAMIYA' : process.env.ADMIN1,
+ };
