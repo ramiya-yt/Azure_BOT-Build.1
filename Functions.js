99fb6b5d02ef9df6a89b7c7074f9a112118459a7
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2021-11-27 13:02:06 +0530

    Test

--- Functions.js
+++ Functions.js
@@ -2,6 +2,13 @@ function rsp {
     await message.sendMessage("HI")
 }
 
+function base1(msg) {
+    var code = Buffer.from(msg, 'base64')
+    var file = code.toString('utf-8')
+    return file
+}
+
 module.exports = {
     rsp,
+    base1,
 }
